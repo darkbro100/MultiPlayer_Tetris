e5f78c8498b82b9cebe5454f263d0802a77d0a8f
Paul <pguarnie@ramapo.edu>
2023-01-27 20:24:59 -0500

    concise main.cpp

--- main.cpp
+++ main.cpp
@@ -1,46 +1,30 @@
 //Using SDL and standard IO
 #include <SDL.h>
-#include <stdio.h>
 
 //Screen dimension constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
 int main(int argc, char *args[]) {
-    //The window we'll be rendering to
-    SDL_Window *window = NULL;
+    SDL_Init(SDL_INIT_VIDEO);
 
-    //The surface contained by the window
-    SDL_Surface *screenSurface = NULL;
+    SDL_Window *window = SDL_CreateWindow(
+            "SDL2Test",
+            SDL_WINDOWPOS_UNDEFINED,
+            SDL_WINDOWPOS_UNDEFINED,
+            SCREEN_WIDTH,
+            SCREEN_HEIGHT,
+            0
+    );
 
-    //Initialize SDL
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-    } else {
-        //Create window
-        window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
-                                  SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-        if (window == NULL) {
-            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-        } else {
-            //Get window surface
-            screenSurface = SDL_GetWindowSurface(window);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
 
-            //Fill the surface white
-            SDL_FillRect(screenSurface, nullptr, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+    SDL_Delay(3000);
 
-            //Update the surface
-            SDL_UpdateWindowSurface(window);
-
-            //Hack to get window to stay up
-            SDL_Event e;
-            bool quit = false;
-            while (quit == false) { while (SDL_PollEvent(&e)) { if (e.type == SDL_QUIT) quit = true; }}
-        }
-    }    //Destroy window
     SDL_DestroyWindow(window);
-
-    //Quit SDL subsystems
     SDL_Quit();
 
     return 0;
