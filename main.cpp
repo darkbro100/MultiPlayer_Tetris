31a9195128f3b8d0b4a13ea3629c927a3595620a
Paul <pguarnie@ramapo.edu>
2023-01-28 18:28:31 -0500

    update

--- main.cpp
+++ main.cpp
@@ -1,31 +1,43 @@
 //Using SDL and standard IO
-#include <SDL.h>
+#include "SDL2-2.26.2/include/SDL.h"
+#include "SDL.h"
+#include <iostream>
 
 //Screen dimension constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-int main(int argc, char *args[]) {
-    SDL_Init(SDL_INIT_VIDEO);
+const int TICKS_PER_SECOND = 60;
+const int SKIP_TICKS = 1000 / TICKS_PER_SECOND;
+const int MAX_FRAMESKIP = 5;
 
-    SDL_Window *window = SDL_CreateWindow(
-            "SDL2Test",
-            SDL_WINDOWPOS_UNDEFINED,
-            SDL_WINDOWPOS_UNDEFINED,
-            SCREEN_WIDTH,
-            SCREEN_HEIGHT,
-            0
-    );
+int main(int argc, char* argv[]) {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        // SDL initialization failed
+        return 1;
+    }
 
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-    SDL_RenderClear(renderer);
-    SDL_RenderPresent(renderer);
+    SDL_Window* window = SDL_CreateWindow("Simple Game Loop", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, 0);
+    if (!window) {
+        // Window creation failed
+        SDL_Quit();
+        return 1;
+    }
 
-    SDL_Delay(3000);
+    bool running = true;
+    SDL_Event event;
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+        }
+        // update game logic here
+        // render game here
+    }
 
     SDL_DestroyWindow(window);
     SDL_Quit();
-
     return 0;
 }
+
